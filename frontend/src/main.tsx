25edb1da066813bfedc4463ba2508293d587e81c
galhadida80 <galhadida80@gmail.com>
2026-02-01 01:33:01 +0200

    auto-claude: subtask-2-2 - Integrate NetworkProvider into app root

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { ThemeProvider } from './theme'
+import { NetworkProvider } from './contexts/NetworkContext'
 import { ToastProvider } from './components/common/ToastProvider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <ToastProvider>
-          <App />
-        </ToastProvider>
+        <NetworkProvider>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+        </NetworkProvider>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
