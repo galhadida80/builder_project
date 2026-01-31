2c91c5987f090780cae16c40b3869c3a7f3f27cd
galhadida80 <galhadida80@gmail.com>
2026-02-01 01:30:50 +0200

    auto-claude: subtask-1-2 - Add /inspector-dashboard route to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,7 @@ import ContactsPage from './pages/ContactsPage'
 import AuditLogPage from './pages/AuditLogPage'
 import InspectionsPage from './pages/InspectionsPage'
 import RFIPage from './pages/RFIPage'
+import InspectorDashboardPage from './pages/InspectorDashboard'
 
 function ProtectedRoute() {
   const token = localStorage.getItem('authToken')
@@ -46,6 +47,7 @@ export default function App() {
 
             <Route path="/approvals" element={<ApprovalsPage />} />
             <Route path="/audit" element={<AuditLogPage />} />
+            <Route path="/inspector-dashboard" element={<InspectorDashboardPage />} />
           </Route>
         </Route>
 
