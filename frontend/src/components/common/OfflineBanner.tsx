c1523d060274b0d5ba71c464e702261fca737acc
galhadida80 <galhadida80@gmail.com>
2026-02-01 01:34:54 +0200

    auto-claude: subtask-3-1 - Create OfflineBanner component with MUI Alert

---
+++ frontend/src/components/common/OfflineBanner.tsx
@@ -0,0 +1,36 @@
+import { styled } from '@mui/material/styles'
+import Alert from '@mui/material/Alert'
+import Slide from '@mui/material/Slide'
+import { useNetwork } from '../../contexts/NetworkContext'
+
+const StyledBannerContainer = styled('div')(({ theme }) => ({
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  right: 0,
+  zIndex: theme.zIndex.snackbar,
+  display: 'flex',
+  justifyContent: 'center',
+  pointerEvents: 'none',
+}))
+
+const StyledAlert = styled(Alert)(({ theme }) => ({
+  width: '100%',
+  borderRadius: 0,
+  pointerEvents: 'auto',
+  boxShadow: theme.shadows[3],
+}))
+
+export function OfflineBanner() {
+  const { isOnline } = useNetwork()
+
+  return (
+    <Slide direction="down" in={!isOnline} mountOnEnter unmountOnExit>
+      <StyledBannerContainer>
+        <StyledAlert severity="warning" variant="filled">
+          You are currently offline. Some features may be unavailable.
+        </StyledAlert>
+      </StyledBannerContainer>
+    </Slide>
+  )
+}
