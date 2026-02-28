7e2054774931442a99b96c3bdbd4f23d388b7646
galhadida80 <galhadida80@gmail.com>
2026-02-28 17:34:38 +0200

    auto-claude: subtask-1-3 - Update models __init__.py to export new models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -56,6 +56,8 @@ from app.models.push_subscription import PushSubscription
 from app.models.scheduled_report import ReportTemplate, ScheduledReport
 from app.models.subcontractor import SubcontractorProfile
 from app.models.subscription import PlanTier, Subscription, SubscriptionPlan
+from app.models.time_entry import TimeEntry
+from app.models.timesheet import Timesheet
 from app.models.webauthn_credential import WebAuthnCredential
 
 __all__ = [
@@ -120,6 +122,8 @@ __all__ = [
     "WebAuthnCredential",
     "Task",
     "TaskDependency",
+    "TimeEntry",
+    "Timesheet",
     "BudgetLineItem",
     "CostEntry",
     "ChangeOrder",
